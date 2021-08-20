9faf9cdc8e6d1f046823a9495d07131a6c503dad
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2021-08-20 12:23:36 +0100

    SW-5026 add error handling

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	logger := log.New(os.Stdout, "opg-sirius-deputy-hub", log.LstdFlags)
+
 	http.HandleFunc("/", HelloServer)
 	http.ListenAndServe(":1234", nil)
+	err := http.ListenAndServe(":1234", nil)
+	if err != nil {
+		logger.Fatalln(err)
+	}
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
