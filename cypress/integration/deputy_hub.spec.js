aa2c8496c35521a1fdcb847aa8f05dbf632f1348
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2021-09-01 09:27:39 +0100

    SW-5027 add footer cypress tests #minor

--- cypress/integration/deputy_hub.spec.js
+++ cypress/integration/deputy_hub.spec.js
@@ -40,4 +40,21 @@ describe("Deputy Hub", () => {
   it("the nav link should contain logout", () => {
     cy.get(".moj-header__navigation-list > :nth-child(4) > a").should("have.attr", "href", "http://localhost:8080/auth/logout")
   }) 
+
+
+  it("the footer should contain a link to the GOV.UK Prototype Kit", () => {
+    cy.get(".govuk-footer__inline-list > :nth-child(1) > a").should("have.attr", "href", "https://govuk-prototype-kit.herokuapp.com/")
+  })  
+  
+  it("the footer should contain a link to clear data", () => {
+    cy.get(".govuk-footer__inline-list > :nth-child(2) > a").should("have.attr", "href", "/prototype-admin/clear-data")
+  })
+  
+  it("the footer should contain a link to the open government licence", () => {
+    cy.get(".govuk-footer__licence-description > .govuk-footer__link").should("have.attr", "href", "https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/")
+  })
+  
+  it("the nav link should contain the crown copyright logo", () => {
+    cy.get(".govuk-footer__copyright-logo").should("have.attr", "href", "https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/")
+  }) 
 });
