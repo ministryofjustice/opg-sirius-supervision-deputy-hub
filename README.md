956aa3d8531f729d41eaceb84b443d7d59f8c064
samuel.joseph <denialanderror@googlemail.com>
2021-09-01 17:23:46 +0100

    SW-5027: Tidies up a few unnused files, renames the generic app to deputy-hub.

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ Alternatively to set it up not using Docker use below. This hosts it on `localho
 
 ## Run Cypress tests
  
-`docker-compose -f docker/docker-compose.cypress.yml up -d --build `
+`docker-compose -f docker/docker-compose.dev.yml up -d --build `
  
 `yarn && yarn cypress `
     
--- docker/puppeteer/lighthouserc.js
+++ docker/puppeteer/lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
       collect: {
-        url: ['http://app:8888/'],
+        url: ['http://deputy-hub:8888/'],
         settings: {
           extraHeaders: JSON.stringify({Cookie: 'XSRF-TOKEN=abcde; Other=other'}),
           chromeFlags: "--disable-gpu --no-sandbox",
@@ -10,7 +10,6 @@ module.exports = {
       assert: {
         assertions: {
           "categories:performance": ["warn", {"minScore": 0.85}],
-          //increase score when sorted out id tags for hook-open-case and selecting elements in pact tests
           "categories:accessibility": ["error", {"minScore": 0.85}],
           "categories:best-practices": ["warn", {"minScore": 0.9}],
           "categories:seo": ["warn", {"minScore": 0.7}],
