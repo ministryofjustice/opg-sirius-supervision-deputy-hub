a0d5fcd3d2b1e6fecfbfe0d0596f3c221f5f73ed
Kate <kate.smith@digital.justice.gov.uk>
2021-09-10 14:09:28 +0100

    sw-4854 start to reformat data

--- internal/sirius/get_deputy_events.go
+++ internal/sirius/get_deputy_events.go
@@ -10,20 +10,30 @@ type DeputyEvents []DeputyEvent
 
 type DeputyEvent struct {
 	TaskId int `json:"id"`
-	//DeputyID               int    `json:"personId"`
-	//DeputyName   string `json:"personName"`
-	//OrganisationName string `json:"organisationName"`
-	//User struct {
-	//	UserId int `json:"id"`
-	//	UserDisplayName int `json:"displayName"`
-	//} `json:"user"`
+	Timestamp string `json:"timestamp"`
+	EventType string `json:"eventType"`
+	DeputyID               int    `json:"personId"`
+	DeputyName   string `json:"personName"`
+	OrganisationName string `json:"organisationName"`
+	User struct {
+		UserId int `json:"id"`
+		UserDisplayName string `json:"displayName"`
+	} `json:"user"`
 	Event struct {
-		OrderNumber string `json:"orderId"`
-		SiriusId string `json:"orderUid"`
 		OrderType string `json:"orderType"`
-		//Client struct {
-		//	ClientName string `json:"personName"`
-		//} `json:"additionalPersons"`
+		SiriusId string `json:"orderUid"`
+		OrderNumber string `json:"orderId"`
+		Changes []struct {
+			FieldName string `json:"fieldName"`
+			OldValue string `json:"oldValue"`
+			NewValue string `json:"newValue"`
+		}`json:"changes"`
+		Client []struct {
+			ClientName string `json:"personName"`
+			ClientId string `json:"personId"`
+			ClientUid string `json:"personUid"`
+			ClientCourtRef string `json:"personCourtRef"`
+		}`json:"additionalPersons"`
 	} `json:"event"`
 }
 
@@ -51,6 +61,37 @@ func (c *Client) GetDeputyEvents(ctx Context, deputyId int) (DeputyEvents, error
 		return v, newStatusError(resp)
 	}
 	err = json.NewDecoder(resp.Body).Decode(&v)
-	return v, err
 
+	DeputyEvents := v
+	DeputyEvents = EditDeputyEvents(v)
+
+	return DeputyEvents, err
+
+}
+
+func EditDeputyEvents(v DeputyEvents) DeputyEvents {
+	var list DeputyEvents
+	for _, s := range v {
+		event := DeputyEvent{
+			Timestamp:        ReformatTimestamp(s),
+			EventType:        ReformatEventType(s),
+			TaskId:           s.TaskId,
+			DeputyID:         s.DeputyID,
+			DeputyName:       s.DeputyName,
+			OrganisationName: s.OrganisationName,
+			User:             s.User,
+			Event: s.Event,
+		}
+
+		list = append(list, event)
+	}
+		return list
+}
+
+func ReformatTimestamp(DeputyEvent) string {
+	return "2020-01-01"
+}
+
+func ReformatEventType(DeputyEvent) string {
+	return "good event"
 }
