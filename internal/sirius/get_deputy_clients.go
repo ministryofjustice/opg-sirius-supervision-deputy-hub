aeb7a3d20a0ef771fbd962e0520ace5650b9390c
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2021-09-15 07:52:28 +0100

    SW-4855 return and display additional client data

--- internal/sirius/get_deputy_clients.go
+++ internal/sirius/get_deputy_clients.go
@@ -6,44 +6,102 @@ import (
 	"net/http"
 )
 
+type Order struct {
+	OrderStatus struct {
+		Label string `json:"label"`
+	}
+	LatestSupervisionLevel struct {
+		SupervisionLevel struct {
+			Label string `json:"label"`
+		}
+	}
+	OrderDate string `json:"orderDate"`
+}
 
-type DeputyClientDetails struct {
-	Clients     []struct {
-		ID          int    `json:"id"`
-		Firstname string `json:"firstname"`
-		Surname	string `json:"surname"`
+type Orders []Order
+
+type apiClients struct {
+	Clients []struct {
+		ID                  int    `json:"id"`
+		Firstname           string `json:"firstname"`
+		Surname             string `json:"surname"`
+		CourtRef            string `json:"caseRecNumber"`
+		RiskScore           int    `json:"riskScore"`
+		ClientAccommodation struct {
+			Label string `json:"label"`
+		}
+		Orders Orders
 	} `json:"persons"`
 }
 
-func (c *Client) GetDeputyClients(ctx Context, deputyId int) (DeputyClientDetails, error) {
-	var v DeputyClientDetails
+type DeputyClient struct {
+	ID                int
+	Firstname         string
+	Surname           string
+	CourtRef          string
+	RiskScore         int
+	AccommodationType string
+	OrderStatus       string
+	SupervisionLevel  string
+}
 
+type DeputyClientDetails []DeputyClient
+
+
+func (c *Client) GetDeputyClients(ctx Context, deputyId int) (DeputyClientDetails, error) {
 	req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/deputies/%d/clients", deputyId), nil)
 
 	if err != nil {
-		return v, err
+		return nil, err
 	}
 
 	resp, err := c.http.Do(req)
 	if err != nil {
-		return v, err
+		return nil, err
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusUnauthorized {
-		return v, ErrUnauthorized
+		return nil, ErrUnauthorized
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return v, newStatusError(resp)
+		return nil, newStatusError(resp)
 	}
-	err = json.NewDecoder(resp.Body).Decode(&v)
 
-	clients := DeputyClientDetails{
-		Clients: v.Clients,
+	var v apiClients
+	if err = json.NewDecoder(resp.Body).Decode(&v); err != nil {
+		return nil, err
+	}
+
+	clients := make(DeputyClientDetails, len(v.Clients))
+
+	for i, t := range v.Clients {
+		clients[i] = DeputyClient{
+			ID:                t.ID,
+			Firstname:         t.Firstname,
+			Surname:           t.Surname,
+			CourtRef:          t.CourtRef,
+			RiskScore:         t.RiskScore,
+			AccommodationType: t.ClientAccommodation.Label,
+			OrderStatus:       CalculateOrderStatus(t.Orders),
+			SupervisionLevel:  GetLatestSupervisionLevel(t.Orders),
+		}
 	}
 
 	return clients, err
 
+}
+
+func CalculateOrderStatus(o Orders) string {
+	// return the status of the oldest active order for a client
+	// if there isn’t one, the status of the oldest order
+
+	return o[0].OrderStatus.Label
+}
+
+func GetLatestSupervisionLevel(o Orders) string {
+
+	return o[0].LatestSupervisionLevel.SupervisionLevel.Label
 }
