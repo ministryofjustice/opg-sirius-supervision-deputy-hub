4ec6a029feb6e81fead104941e244eb4c75debff
Kate <kate.smith@digital.justice.gov.uk>
2021-09-22 09:33:21 +0100

    sw-4864 fix cypress test and add 2 more for new tabs

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -8,6 +8,10 @@ describe("Navigation bar", () => {
   it("has working nav links for different tabs", () => {
       cy.get(".moj-sub-navigation__list > :nth-child(1) > a").should("contain", "Dashboard");
       cy.get(".moj-sub-navigation__list > :nth-child(1) > a").should("have.attr", "href", "/supervision/deputies/public-authority/deputy/1/");
+      cy.get(".moj-sub-navigation__list > :nth-child(2) > a").should("contain", "Timeline");
+      cy.get(".moj-sub-navigation__list > :nth-child(2) > a").should("have.attr", "href", "/supervision/deputies/public-authority/deputy/1/timeline");
+      cy.get(".moj-sub-navigation__list > :nth-child(3) > a").should("contain", "Notes");
+      cy.get(".moj-sub-navigation__list > :nth-child(3) > a").should("have.attr", "href", "/supervision/deputies/public-authority/deputy/1/notes");
   })
 
 });
