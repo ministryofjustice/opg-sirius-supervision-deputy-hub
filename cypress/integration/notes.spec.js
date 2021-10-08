0eadea1344d7769c5507de71bb7581eb9fcb89a6
Mishkat Najam <mishkat.najam@digital.justice.gov.uk>
2021-10-08 15:35:54 +0100

    SW-4865 use id in cypress test

--- cypress/integration/notes.spec.js
+++ cypress/integration/notes.spec.js
@@ -44,8 +44,7 @@ describe("Notes", () => {
             cy.visit("/supervision/deputies/public-authority/deputy/1/notes/add-note");
             cy.get("#title").type("title")
             cy.get("#note").type("note")
-            cy.get(".govuk-button").should("contain", "Save note")
-            cy.get(".govuk-button").click()
+            cy.get('#add-note-form').submit()
             cy.url().should("contain", "/supervision/deputies/public-authority/deputy/1/notes");
             cy.get("body > div > main > div.moj-banner.moj-banner--success > div").should("contain", "Note added");
         })
@@ -54,8 +53,7 @@ describe("Notes", () => {
             cy.visit("/supervision/deputies/public-authority/deputy/1/notes/add-note");
             cy.get("#title").type("New note title")
             cy.get("#note").type("Note text entered")
-            cy.get(".govuk-button").should("contain", "Save note")
-            cy.get(".govuk-button").click()
+            cy.get('#add-note-form').submit()
             cy.url().should("contain", "/supervision/deputies/public-authority/deputy/1/notes");
             cy.get(":nth-last-child(1) > .moj-timeline__header > .moj-timeline__title").should("contain", "New note title");
             cy.get(':nth-last-child(1) > .moj-timeline__description').should("contain", "Note text entered");
