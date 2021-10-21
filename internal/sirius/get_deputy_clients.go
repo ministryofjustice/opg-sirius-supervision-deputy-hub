2bf5523ae668da215e3a0e145cbba580968247ec
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2021-10-21 15:41:45 +0100

    sw-4857 refactoring date sort #minor

--- internal/sirius/get_deputy_clients.go
+++ internal/sirius/get_deputy_clients.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"reflect"
 	"sort"
 	"strings"
 	"time"
@@ -122,6 +121,7 @@ func (c *Client) GetDeputyClients(ctx Context, deputyId int, columnBeingSorted s
 				SupervisionLevel:  GetMostRecentSupervisionLevel(orders),
 				OldestReport:      reportReturned{t.OldestReport.DueDate, t.OldestReport.RevisedDueDate, t.OldestReport.Status.Label},
 			}
+
 			clients = append(clients, client)
 		}
 	}
@@ -237,6 +237,10 @@ func CrecScoreSort(clients DeputyClientDetails, sortOrder string) DeputyClientDe
 }
 
 func SetDueDateForSort(dueDate, revisedDueDate string) string {
+    if dueDate == "" && revisedDueDate == "" {
+       return "00/00/0001"
+    }
+
 	if revisedDueDate != "" {
 		return revisedDueDate
 	} else {
@@ -244,150 +248,28 @@ func SetDueDateForSort(dueDate, revisedDueDate string) string {
 	}
 }
 
+func FormattingDate(dateString string) time.Time {
+    dateArray := strings.Split(dateString, "/")
+    reformattedDate := dateArray[2] + "-" + dateArray[1] + "-" + dateArray[0]
+    dateTime, _ := time.Parse("2006-01-02", reformattedDate)
+    return dateTime
+}
+
 func ReportDueScoreSort(clients DeputyClientDetails, sortOrder string) DeputyClientDetails {
-	fmt.Println("before")
-	fmt.Println(clients)
 	sort.Slice(clients, func(i, j int) bool {
 		x := SetDueDateForSort(clients[i].OldestReport.DueDate, clients[i].OldestReport.RevisedDueDate)
 		y := SetDueDateForSort(clients[j].OldestReport.DueDate, clients[j].OldestReport.RevisedDueDate)
+        dateTimeI := FormattingDate(x)
+        dateTimeJ := FormattingDate(y)
 
-		fmt.Println("x")
-		fmt.Println(x)
-		fmt.Println("y")
-		fmt.Println(y)
-
-		clientiReportDueDateArray := strings.Split(x, "/")
-		fmt.Println("client i report revised array")
-		fmt.Println(clientiReportDueDateArray)
-		fmt.Println("check array value")
-		fmt.Println(reflect.ValueOf(clientiReportDueDateArray).Kind())
-		fmt.Println("element 0")
-		fmt.Println(clientiReportDueDateArray[0])
-		fmt.Println("element 1")
-		fmt.Println(clientiReportDueDateArray[1])
-		fmt.Println("element 2")
-		fmt.Println(clientiReportDueDateArray[2])
-		fmt.Println("split the array")
-		fmt.Println(clientiReportDueDateArray)
-		clientiRestructuredDueDate := clientiReportDueDateArray[2] + "-" + clientiReportDueDateArray[1] + "-" + clientiReportDueDateArray[0]
-		fmt.Println("did i get past the using split bit")
-		clientjReportDueDate := clients[j].OldestReport.DueDate
-		fmt.Println("client j report revised due date")
-		fmt.Println(clientjReportDueDate)
-		clientjReportDueDateArray := strings.Split(y, "/")
-		clientjRestructuredDueDate := clientjReportDueDateArray[2] + "-" + clientjReportDueDateArray[1] + "-" + clientjReportDueDateArray[0]
-		iDueDateTime, _ := time.Parse("2006-01-02", clientiRestructuredDueDate)
-		jDueDateTime, _ := time.Parse("2006-01-02", clientjRestructuredDueDate)
-		fmt.Println("iDueDateTime")
-		fmt.Println(iDueDateTime)
-		fmt.Println("jDueDateTime")
-		fmt.Println(jDueDateTime)
 		if sortOrder == "asc" {
-			return iDueDateTime.Before(jDueDateTime)
+			return dateTimeI.Before(dateTimeJ)
 		} else {
-			return jDueDateTime.Before(iDueDateTime)
+			return dateTimeJ.Before(dateTimeI)
 		}
 	})
-	fmt.Println("after")
-	fmt.Println(clients)
 	return clients
 }
-
-// func formatDate(unformattedDate string) string {
-// 	// log date to see if null
-// 	fmt.Println("unformatted date at start of format date")
-// 	fmt.Println(unformattedDate)
-// 	unformattedDateArray := strings.Split(unformattedDate, "/")
-// 	fmt.Println("Unformatted date array is")
-// 	fmt.Println(unformattedDateArray)
-// 	var formattedDate string
-// 	fmt.Println("Unformatted date array length is")
-// 	fmt.Println(len(unformattedDateArray))
-// 	if len(unformattedDateArray) > 2 {
-// 		formattedDate = unformattedDateArray[2] + "-" + unformattedDateArray[1] + "-" + unformattedDateArray[0]
-// 		// formattedDate, _ = time.Parse("2006-01-02", restructuredDate)
-// 		fmt.Println("formattedDate is")
-// 		fmt.Println(formattedDate)
-// 		return formattedDate
-// 	} else {
-// 		return formattedDate
-// 	}
-
-// 	return formattedDate
-
-// }
-
-// func compareDates(sortOrder string, iDueDate, jDueDate time.Time) bool {
-// 	if sortOrder == "asc" {
-// 		return iDueDate.Before(jDueDate)
-// 	} else {
-// 		return jDueDate.Before(iDueDate)
-// 	}
-// }
-
-// func ReportDueScoreSort(clients DeputyClientDetails, sortOrder string) DeputyClientDetails {
-// 	sort.Slice(clients, func(i, j int) bool {
-// 		fmt.Println("client is")
-// 		fmt.Println(clients[i].Firstname)
-// 		fmt.Println("due date is ")
-// 		fmt.Println(clients[i].OldestReport.DueDate)
-// 		fmt.Println("revised due date is ")
-// 		fmt.Println(clients[i].OldestReport.RevisedDueDate)
-// 		fmt.Println("type of due date")
-// 		fmt.Printf("%T", clients[i].OldestReport.DueDate)
-// 		if clients[i].OldestReport.RevisedDueDate != "null" {
-// 			clientiDueDate := formatDate(clients[i].OldestReport.RevisedDueDate)
-// 			fmt.Println("client i due date after format date")
-// 			fmt.Println(clientiDueDate)
-// 			clientiDueDateTime, _ := time.Parse("2006-01-02", clientiDueDate)
-// 			fmt.Println("client i due date time within report due score sort")
-// 			fmt.Println(clientiDueDateTime)
-// 			if clients[j].OldestReport.RevisedDueDate != "null" {
-// 				clientjDueDate := formatDate(clients[j].OldestReport.RevisedDueDate)
-// 				fmt.Println("client j due date after format date")
-// 				fmt.Println(clientjDueDate)
-// 				clientjDueDateTime, _ := time.Parse("2006-01-02", clientjDueDate)
-// 				fmt.Println("client i due date time within report due score sort")
-// 				fmt.Println(clientjDueDateTime)
-// 				return compareDates(sortOrder, clientiDueDateTime, clientjDueDateTime)
-// 			} else {
-// 				clientjDueDate := formatDate(clients[j].OldestReport.DueDate)
-// 				fmt.Println("client j due date after format date")
-// 				fmt.Println(clientjDueDate)
-// 				clientjDueDateTime, _ := time.Parse("2006-01-02", clientjDueDate)
-// 				fmt.Println("client j due date time within report due score sort")
-// 				fmt.Println(clientjDueDateTime)
-// 				return compareDates(sortOrder, clientiDueDateTime, clientjDueDateTime)
-// 			}
-// 		} else {
-// 			clientiDueDate := formatDate(clients[i].OldestReport.DueDate)
-// 			fmt.Println("client i due date after format date")
-// 			fmt.Println(clientiDueDate)
-// 			clientiDueDateTime, _ := time.Parse("2006-01-02", clientiDueDate)
-// 			fmt.Println("client i due date time within report due score sort")
-// 			fmt.Println(clientiDueDateTime)
-// 			if clients[j].OldestReport.RevisedDueDate != "null" {
-
-// 				clientjDueDate := formatDate(clients[j].OldestReport.RevisedDueDate)
-// 				fmt.Println("client j due date after format date")
-// 				fmt.Println(clientjDueDate)
-// 				clientjDueDateTime, _ := time.Parse("2006-01-02", clientjDueDate)
-// 				fmt.Println("client j due date within report due score sort")
-// 				fmt.Println(clientjDueDate)
-// 				return compareDates(sortOrder, clientiDueDateTime, clientjDueDateTime)
-// 			} else {
-// 				clientjDueDate := formatDate(clients[j].OldestReport.DueDate)
-// 				fmt.Println("client j due date after format date")
-// 				fmt.Println(clientjDueDate)
-// 				clientjDueDateTime, _ := time.Parse("2006-01-02", clientjDueDate)
-// 				fmt.Println("client j due date time within report due score sort")
-// 				fmt.Println(clientjDueDateTime)
-// 				return compareDates(sortOrder, clientiDueDateTime, clientjDueDateTime)
-// 			}
-// 		}
-// 	})
-// 	return clients
-// }
 
 func ChangeSortButtonDirection(sortOrder string, columnBeingSorted string, functionCalling string) string {
 	if functionCalling == columnBeingSorted {
