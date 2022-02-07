f4309436ee86a66aa99b3ea2cc49b43a2103ecfd
Kate <kate.smith@digital.justice.gov.uk>
2022-02-07 15:31:49 +0000

    sw-5441 added cypress test

--- cypress/integration/manage_important_information.spec.js
+++ cypress/integration/manage_important_information.spec.js
@@ -30,7 +30,7 @@ describe("Manage important Information", () => {
         });
     });
 
-    describe("Manage important information form", () => {
+    describe("Manage Pro Deputy important information form", () => {
         beforeEach(() => {
             cy.visit("/supervision/deputies/3/manage-important-information");
         });
@@ -43,7 +43,7 @@ describe("Manage important Information", () => {
         });
 
         it("allows me to edit and submit the form", () => {
-            cy.setCookie("success-route", "importantInformation");
+            cy.setCookie("success-route", "proImportantInformation");
             cy.get("#complaints-No").click();
             cy.get("#panel-deputy-no").click();
             cy.get("#annual-billing-Invoice").click();
@@ -72,7 +72,7 @@ describe("Manage important Information", () => {
         });
     });
 
-    describe("Default values", () => {
+    describe("Manage Pro Deputy default values", () => {
         beforeEach(() => {
             cy.visit("/supervision/deputies/4/manage-important-information");
         });
@@ -84,5 +84,51 @@ describe("Manage important Information", () => {
             cy.get("#other-info-note").should('have.text', "");
         });
 
+    });
+
+    describe("Manage Pa Deputy important information form", () => {
+        beforeEach(() => {
+            cy.visit("/supervision/deputies/1/manage-important-information");
+        });
+
+        it("autofills in existing data", () => {
+            cy.get("#monthly-spreadsheet-No").should('be.checked');
+            cy.get("#independent-visitor-charges-Unknown").should('be.checked');
+            cy.get("#bank-charges-Yes").should('be.checked');
+            cy.get("#apad-Yes").should('be.checked');
+            cy.get("#report-system-CASHFAC").should('be.checked');
+            cy.get("#annual-billing-Schedule\\ and\\ Invoice").should('be.checked');
+            cy.get("#other-info-note").should('have.text', "some info for the pa deputy");
+        });
+
+        it("allows me to edit and submit the form", () => {
+            cy.setCookie("success-route", "paImportantInformation");
+            cy.get("#independent-visitor-charges-No").click();
+            cy.get("#bank-charges-No").click();
+            cy.get("#apad-Unknown").click();
+            cy.get("#annual-billing-Invoice").click();
+            cy.get("#other-info-note").clear().type("new data entered into box");
+            cy.get(".govuk-button").click()
+            cy.url().should(
+                "not.contain",
+                "/supervision/deputies/3/manage-important-information"
+            );
+            cy.get(".govuk-heading-l").should("contain", "Deputy details");
+            cy.get(".moj-banner").should("contain", "Important information updated")
+        });
+
+        it("will show validation errors", () => {
+            cy.setCookie("fail-route", "importantInformation");
+            cy.get("#other-info-note").clear().type("data that is too long for the box");
+            cy.get(".govuk-button").click();
+            cy.get(".govuk-error-summary__title").should(
+                "contain",
+                "There is a problem"
+            );
+            cy.get(".govuk-list > li > a").should(
+                "contain",
+                "The other important information must be 1000 characters or fewer"
+            );
+        });
     });
 });
