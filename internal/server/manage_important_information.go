ac5ae592195731ead503237c74b045536f0c1394
Kate Smith <52196833+kate-49@users.noreply.github.com>
2022-02-10 10:17:36 +0000

    Sw 5441 important info pa (#60)
    * sw-5441 put in lots of the files and no errors
    
    * sw-5441 renders on pa and pro templates and structs created
    
    * sw-5441 second set of forms work for selecting values
    
    * sw-5441 pro details works with existing api functionality and shows timeline events
    
    * sw-5441 fix page not rendering errors
    
    * sw-5441 end of thursday
    
    * sw-5426 pro works and added get report status types in;
    
    * sw-5441 hits ticket criteria, tests and linted;
    
    * sw-5441 added cypress test
    
    * sw-5441 fix cypress test
    
    * sw-5441 cleaned up tests
    
    * sw-5441 slightly amended template spacing
    
    * sw-5441 moved sirius api calls into 1 call

---
+++ internal/server/manage_important_information.go
@@ -0,0 +1,151 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/ministryofjustice/opg-sirius-supervision-deputy-hub/internal/sirius"
+)
+
+type ManageProDeputyImportantInformation interface {
+	GetDeputyDetails(sirius.Context, int, int) (sirius.DeputyDetails, error)
+	UpdateImportantInformation(sirius.Context, int, sirius.ImportantInformationDetails) error
+	GetDeputyAnnualInvoiceBillingTypes(ctx sirius.Context) ([]sirius.DeputyAnnualBillingInvoiceTypes, error)
+	GetDeputyBooleanTypes(ctx sirius.Context) ([]sirius.DeputyBooleanTypes, error)
+	GetDeputyReportSystemTypes(ctx sirius.Context) ([]sirius.DeputyReportSystemTypes, error)
+}
+
+type manageDeputyImportantInformationVars struct {
+	Path                      string
+	XSRFToken                 string
+	DeputyDetails             sirius.DeputyDetails
+	Error                     string
+	Errors                    sirius.ValidationErrors
+	DeputyId                  int
+	AnnualBillingInvoiceTypes []sirius.DeputyAnnualBillingInvoiceTypes
+	DeputyBooleanTypes        []sirius.DeputyBooleanTypes
+	DeputyReportSystemTypes   []sirius.DeputyReportSystemTypes
+}
+
+func renderTemplateForImportantInformation(client ManageProDeputyImportantInformation, defaultPATeam int, tmpl Template) Handler {
+	return func(perm sirius.PermissionSet, w http.ResponseWriter, r *http.Request) error {
+
+		ctx := getContext(r)
+		routeVars := mux.Vars(r)
+		deputyId, _ := strconv.Atoi(routeVars["id"])
+
+		deputyDetails, err := client.GetDeputyDetails(ctx, defaultPATeam, deputyId)
+		if err != nil {
+			return err
+		}
+
+		annualBillingInvoiceTypes, err := client.GetDeputyAnnualInvoiceBillingTypes(ctx)
+		if err != nil {
+			return err
+		}
+
+		deputyBooleanTypes, err := client.GetDeputyBooleanTypes(ctx)
+		if err != nil {
+			return err
+		}
+
+		deputyReportSystemTypes, err := client.GetDeputyReportSystemTypes(ctx)
+		if err != nil {
+			return err
+		}
+
+		switch r.Method {
+		case http.MethodGet:
+
+			vars := manageDeputyImportantInformationVars{
+				Path:                      r.URL.Path,
+				XSRFToken:                 ctx.XSRFToken,
+				DeputyId:                  deputyId,
+				DeputyDetails:             deputyDetails,
+				AnnualBillingInvoiceTypes: annualBillingInvoiceTypes,
+				DeputyBooleanTypes:        deputyBooleanTypes,
+				DeputyReportSystemTypes:   deputyReportSystemTypes,
+			}
+			return tmpl.ExecuteTemplate(w, "page", vars)
+
+		case http.MethodPost:
+
+			var panelDeputyBool bool
+
+			if r.PostFormValue("panel-deputy") != "" {
+				panelDeputyBool, err = strconv.ParseBool(r.PostFormValue("panel-deputy"))
+				if err != nil {
+					return err
+				}
+			}
+			reportSystemType := checkForReportSystemType(r.PostFormValue("report-system"))
+
+			importantInfoForm := sirius.ImportantInformationDetails{
+				DeputyType:                deputyDetails.DeputyType.Handle,
+				Complaints:                r.PostFormValue("complaints"),
+				PanelDeputy:               panelDeputyBool,
+				AnnualBillingInvoice:      r.PostFormValue("annual-billing"),
+				OtherImportantInformation: r.PostFormValue("other-info-note"),
+				MonthlySpreadsheet:        r.PostFormValue("monthly-spreadsheet"),
+				IndependentVisitorCharges: r.PostFormValue("independent-visitor-charges"),
+				BankCharges:               r.PostFormValue("bank-charges"),
+				APAD:                      r.PostFormValue("apad"),
+				ReportSystem:              reportSystemType,
+			}
+
+			err = client.UpdateImportantInformation(ctx, deputyId, importantInfoForm)
+
+			if verr, ok := err.(sirius.ValidationError); ok {
+				verr.Errors = renameUpdateAdditionalInformationValidationErrorMessages(verr.Errors)
+
+				vars := manageDeputyImportantInformationVars{
+					Path:                      r.URL.Path,
+					XSRFToken:                 ctx.XSRFToken,
+					DeputyId:                  deputyId,
+					DeputyDetails:             deputyDetails,
+					Errors:                    verr.Errors,
+					AnnualBillingInvoiceTypes: annualBillingInvoiceTypes,
+					DeputyBooleanTypes:        deputyBooleanTypes,
+					DeputyReportSystemTypes:   deputyReportSystemTypes,
+				}
+				return tmpl.ExecuteTemplate(w, "page", vars)
+			} else if err != nil {
+				return err
+			}
+
+			return Redirect(fmt.Sprintf("/%d?success=importantInformation", deputyId))
+		default:
+			return StatusError(http.StatusMethodNotAllowed)
+		}
+	}
+}
+
+func renameUpdateAdditionalInformationValidationErrorMessages(siriusError sirius.ValidationErrors) sirius.ValidationErrors {
+	errorCollection := sirius.ValidationErrors{}
+
+	for fieldName, value := range siriusError {
+		for errorType, errorMessage := range value {
+			err := make(map[string]string)
+			if fieldName == "otherImportantInformation" && errorType == "stringLengthTooLong" {
+				err[errorType] = "The other important information must be 1000 characters or fewer"
+				errorCollection["otherImportantInformation"] = err
+			} else {
+				err[errorType] = errorMessage
+				errorCollection[fieldName] = err
+			}
+		}
+	}
+	return errorCollection
+}
+
+func checkForReportSystemType(reportType string) string {
+	if reportType == "OPG Digital" {
+		return "OPGDigital"
+	} else if reportType == "OPG Paper" {
+		return "OPGPaper"
+	} else {
+		return reportType
+	}
+}
