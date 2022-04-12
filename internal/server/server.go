99362a94cf49afc2182afee9b4f9a5fc3ced461a
Dimitar Kokov <dm.kokov@gmail.com>
2022-04-12 10:50:13 +0100

    SW-5596 Configurable FIRM_HUB_HOST for local development (#99)

--- internal/server/server.go
+++ internal/server/server.go
@@ -34,7 +34,7 @@ type Template interface {
 	ExecuteTemplate(io.Writer, string, interface{}) error
 }
 
-func New(logger Logger, client Client, templates map[string]*template.Template, prefix, siriusPublicURL, firmHubURL, webDir string, defaultPATeam int) http.Handler {
+func New(logger Logger, client Client, templates map[string]*template.Template, prefix, siriusPublicURL, webDir string, defaultPATeam int) http.Handler {
 	wrap := errorHandler(logger, client, templates["error.gotmpl"], prefix, siriusPublicURL)
 
 	router := mux.NewRouter()
