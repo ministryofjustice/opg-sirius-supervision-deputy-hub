262d7cbd01544e23eaafc324bc1b383535108f71
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2022-07-21 12:52:00 +0100

    SW-4856 add more test coverage

--- internal/sirius/get_page_details_test.go
+++ internal/sirius/get_page_details_test.go
@@ -1,6 +1,7 @@
 package sirius
 
 import (
+	"github.com/ministryofjustice/opg-sirius-supervision-deputy-hub/internal/mocks"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,6 +24,54 @@ func SetUpGetNextPageNumber(pageCurrent int, pageTotal int, totalClients int) Cl
 		TotalClients: totalClients,
 	}
 	return clientList
+}
+
+func TestGetPageDetailsReturned(t *testing.T) {
+	mockClient := &mocks.MockClient{}
+	client, _ := NewClient(mockClient, "http://localhost:3000")
+
+	clientList := SetUpGetNextPageNumber(1, 1, 1)
+
+	expectedResponse := PageDetails{
+		ListOfPages:       []int{1},
+		PreviousPage:      1,
+		NextPage:          1,
+		LimitedPagination: []int{1},
+		FirstPage:         1,
+		LastPage:          1,
+		StoredClientLimit: 25,
+		ShowingUpperLimit: 1,
+		ShowingLowerLimit: 1,
+		LastFilter:        "",
+	}
+
+	pageDetails := client.GetPageDetails(getContext(nil), clientList, 1, 25)
+
+	assert.Equal(t, expectedResponse, pageDetails)
+}
+
+func TestGetPageDetailsReturnedWithNoClients(t *testing.T) {
+	mockClient := &mocks.MockClient{}
+	client, _ := NewClient(mockClient, "http://localhost:3000")
+
+	clientList := SetUpGetNextPageNumber(1, 0, 0)
+
+	expectedResponse := PageDetails{
+		ListOfPages:       []int(nil),
+		PreviousPage:      1,
+		NextPage:          0,
+		LimitedPagination: []int{0},
+		FirstPage:         0,
+		LastPage:          0,
+		StoredClientLimit: 25,
+		ShowingUpperLimit: 0,
+		ShowingLowerLimit: 0,
+		LastFilter:        "",
+	}
+
+	pageDetails := client.GetPageDetails(getContext(nil), clientList, 1, 25)
+
+	assert.Equal(t, expectedResponse, pageDetails)
 }
 
 func TestGetNextPageNumber(t *testing.T) {
@@ -71,4 +120,39 @@ func TestGetShowingUpperLimitNumberWillReturnTotalClientsIfOnFinalPage(t *testin
 	assert.Equal(t, GetShowingUpperLimitNumber(clientList, 25), 10)
 	assert.Equal(t, GetShowingUpperLimitNumber(clientList, 50), 10)
 	assert.Equal(t, GetShowingUpperLimitNumber(clientList, 100), 10)
+}
+
+func TestGetShowingUpperLimitNumberWillIncrementOnManyPages(t *testing.T) {
+	clientList := SetUpGetNextPageNumber(1, 0, 1000)
+
+	assert.Equal(t, GetShowingUpperLimitNumber(clientList, 25), 25)
+	assert.Equal(t, GetShowingUpperLimitNumber(clientList, 50), 50)
+	assert.Equal(t, GetShowingUpperLimitNumber(clientList, 100), 100)
+}
+
+func TestGetPaginationLimitsWhenOnFirstPage(t *testing.T) {
+	clientList := SetUpGetNextPageNumber(1, 0, 25)
+	clientDetails := PageDetails{
+		ListOfPages: []int{1, 2, 3, 4},
+		LastPage:    4,
+	}
+	assert.Equal(t, GetPaginationLimits(clientList, clientDetails), []int{1, 2, 3})
+}
+
+func TestGetPaginationLimitsWhenOnSecondToLastPage(t *testing.T) {
+	clientList := SetUpGetNextPageNumber(3, 0, 25)
+	clientDetails := PageDetails{
+		ListOfPages: []int{1, 2, 3, 4},
+		LastPage:    4,
+	}
+	assert.Equal(t, GetPaginationLimits(clientList, clientDetails), []int{1, 2, 3, 4})
+}
+
+func TestGetPaginationLimitsWhenOnLastPage(t *testing.T) {
+	clientList := SetUpGetNextPageNumber(4, 0, 25)
+	clientDetails := PageDetails{
+		ListOfPages: []int{1, 2, 3, 4},
+		LastPage:    4,
+	}
+	assert.Equal(t, GetPaginationLimits(clientList, clientDetails), []int{2, 3, 4})
 }
