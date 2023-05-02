2f8869555ae541c214c2b004cb92256f1a40e537
Kate <kate.smith@digital.justice.gov.uk>
2023-05-02 14:46:03 +0100

    sw-6550 set local time and amend tests

--- internal/sirius/format_date_and_time.go
+++ internal/sirius/format_date_and_time.go
@@ -1,6 +1,8 @@
 package sirius
 
-import "time"
+import (
+	"time"
+)
 
 const DateTimeFormat string = "2006-01-02T15:04:05+00:00"
 const DateTimeDisplayFormat string = "2006-01-02"
@@ -10,6 +12,10 @@ func FormatDateAndTime(formatForDateTime string, dateString string, displayLayou
 		return dateString
 	}
 	stringToDateTime, _ := time.Parse(formatForDateTime, dateString)
-	dateTime := stringToDateTime.Format(displayLayoutDateTime)
+	dateTime := stringToDateTime.Local().Format(displayLayoutDateTime)
 	return dateTime
+}
+
+func isDST() bool {
+	return time.Now().Local() != time.Now()
 }
--- internal/sirius/format_date_and_time_test.go
+++ internal/sirius/format_date_and_time_test.go
@@ -8,6 +8,13 @@ import (
 
 func TestFormatDateAndTime(t *testing.T) {
 	unsortedData := "2020-10-18 10:11:08"
-	expectedResponse := "18/10/2020 10:11:08"
-	assert.Equal(t, expectedResponse, FormatDateAndTime("2006-01-02 15:04:05", unsortedData, "02/01/2006 15:04:05"))
+	expectedResponse := ""
+
+	if isDST() {
+		expectedResponse = "18/10/2020 11:11:08"
+	} else {
+		expectedResponse = "18/10/2020 10:11:08"
+	}
+
+	assert.Equal(t, expectedResponse, FormatDateAndTime(TimelineDateTimeFormat, unsortedData, TimelineDateTimeDisplayFormat))
 }
