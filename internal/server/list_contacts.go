f52b62816e9f486a73680f30653de90ace292e52
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2023-07-11 12:23:49 +0100

    SW-5159 Added get deputy contacts file

--- internal/server/list_contacts.go
+++ internal/server/list_contacts.go
@@ -2,25 +2,53 @@ package server
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/ministryofjustice/opg-sirius-supervision-deputy-hub/internal/sirius"
 )
+
+type DeputyHubContactInformation interface {
+	GetDeputyContacts(sirius.Context, int, int, int, string, string, string) (sirius.ContactList, sirius.AriaSorting, error)
+	// GetPageDetails(sirius.Context, sirius.ContactList, int, int) sirius.PageDetails
+}
 
 type ListContactsVars struct {
 	Path                 string
 	XSRFToken            string
+	AriaSorting          sirius.AriaSorting
 	DeputyDetails        sirius.DeputyDetails
+	ContactList			 sirius.ContactList
+	PageDetails          sirius.PageDetails
 	SuccessMessage       string
 	Error                string
 }
 
-func renderTemplateForContactTab(client DeputyHubClientInformation, tmpl Template) Handler {
+func renderTemplateForContactTab(client DeputyHubContactInformation, tmpl Template) Handler {
 	return func(deputyDetails sirius.DeputyDetails, w http.ResponseWriter, r *http.Request) error {
 		if r.Method != http.MethodGet {
 			return StatusError(http.StatusMethodNotAllowed)
 		}
 
 		ctx := getContext(r)
+		routeVars := mux.Vars(r)
+		urlParams := r.URL.Query()
+
+		deputyId, _ := strconv.Atoi(routeVars["id"])
+		search, _ := strconv.Atoi(r.FormValue("page"))
+		displayContactLimit, _ := strconv.Atoi(r.FormValue("limit"))
+		if displayContactLimit == 0 {
+			displayContactLimit = 25
+		}
+
+		columnBeingSorted, sortOrder := parseUrl(urlParams)
+
+		contactList, ariaSorting, err := client.GetDeputyContacts(ctx, deputyId, displayContactLimit, search, deputyDetails.DeputyType.Handle, columnBeingSorted, sortOrder)
+		if err != nil {
+			return err
+		}
+
+		// pageDetails := client.GetPageDetails(ctx, contactList, search, displayContactLimit)
 
 		var successMessage string
 		switch r.URL.Query().Get("success") {
@@ -33,6 +61,9 @@ func renderTemplateForContactTab(client DeputyHubClientInformation, tmpl Templat
 		vars := ListContactsVars{
 			Path:                 r.URL.Path,
 			XSRFToken:            ctx.XSRFToken,
+			AriaSorting:          ariaSorting,
+			ContactList:          contactList,
+			// PageDetails:          pageDetails,
 			DeputyDetails:        deputyDetails,
 			SuccessMessage:       successMessage,
 		}
---
+++ internal/sirius/get_deputy_contacts.go
@@ -0,0 +1,106 @@
+package sirius
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	
+	"strings"
+	
+)
+
+type apiContact struct {
+	ContactId        int    `json:"id"`
+	Name             string `json:"name"`
+	JobTitle         string `json:"jobTitle"`
+	Email            string `json:"email"`
+	PhoneNumber      string `json:"phoneNumber"`
+	OtherPhoneNumber string `json:"otherPhoneNumber"`
+	ContactNotes     string `json:"contactNotes"`
+	IsMainContact    bool   `json:"isMainContact"`
+	IsNamedDeputy    bool   `json:"isNamedDeputy"`
+}
+
+type ApiContactList struct {
+	Contacts      []apiContact `json:"contacts"`
+	Pages         Page         `json:"pages"`
+	Metadata      Metadata     `json:"metadata"`
+	TotalContacts int          `json:"total"`
+}
+
+type ContactList struct {
+	Contacts      DeputyContactsDetails
+	Pages         Page
+	TotalContacts int
+	Metadata      Metadata
+}
+
+type DeputyContact struct {
+	ContactId        int
+	Name             string
+	JobTitle         string
+	Email            string
+	PhoneNumber      string
+	OtherPhoneNumber string
+	ContactNotes     string
+	IsMainContact    bool
+	IsNamedDeputy    bool
+}
+
+type DeputyContactsDetails []DeputyContact
+
+func (c *Client) GetDeputyContacts(ctx Context, deputyId, displayContactLimit, search int, deputyType, columnBeingSorted, sortOrder string) (ContactList, AriaSorting, error) {
+	var contactList ContactList
+	var apiContactList ApiContactList
+
+	req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/deputies/%s/%d/contacts?&limit=%d&page=%d", strings.ToLower(deputyType), deputyId, displayContactLimit, search), nil)
+
+	if err != nil {
+		return contactList, AriaSorting{}, err
+	}
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return contactList, AriaSorting{}, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		return contactList, AriaSorting{}, ErrUnauthorized
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return contactList, AriaSorting{}, newStatusError(resp)
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(&apiContactList); err != nil {
+		return contactList, AriaSorting{}, err
+	}
+
+	var contacts DeputyContactsDetails
+	for _, t := range apiContactList.Contacts {
+
+			var contact = DeputyContact{
+				ContactId:        t.ContactId,
+				Name:             t.Name,
+				JobTitle:         t.JobTitle,
+				Email:            t.Email,
+				PhoneNumber:      t.PhoneNumber,
+				OtherPhoneNumber: t.OtherPhoneNumber,
+				ContactNotes:     t.ContactNotes,
+				IsMainContact:    t.IsMainContact,
+				IsNamedDeputy:    t.IsNamedDeputy,
+			}
+			
+			contacts = append(contacts, contact)
+	}
+	contactList.Contacts = contacts
+
+	var aria AriaSorting
+	aria.SurnameAriaSort = changeSortButtonDirection(sortOrder, columnBeingSorted, "surname")
+	aria.ReportDueAriaSort = changeSortButtonDirection(sortOrder, columnBeingSorted, "reportdue")
+	aria.CRECAriaSort = changeSortButtonDirection(sortOrder, columnBeingSorted, "crec")
+
+	return contactList, aria, err
+}
