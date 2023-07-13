e86a2a75e6d9d4607755d225675af18f67f71439
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2023-07-13 16:10:45 +0100

    SW-5159 Created add contact template

--- internal/sirius/get_deputy_contacts.go
+++ internal/sirius/get_deputy_contacts.go
@@ -4,23 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	
 )
 
-type apiContact struct {
-	ContactId        int    `json:"id"`
+type ApiContact struct {
+	Id               int    `json:"id"`
 	Name             string `json:"name"`
 	JobTitle         string `json:"jobTitle"`
 	Email            string `json:"email"`
 	PhoneNumber      string `json:"phoneNumber"`
 	OtherPhoneNumber string `json:"otherPhoneNumber"`
-	ContactNotes     string `json:"contactNotes"`
+	Notes            string `json:"notes"`
 	IsMainContact    bool   `json:"isMainContact"`
 	IsNamedDeputy    bool   `json:"isNamedDeputy"`
 }
 
 type ApiContactList struct {
-	Contacts      []apiContact `json:"contacts"`
+	Contacts      []ApiContact `json:"contacts"`
 	Pages         Page         `json:"pages"`
 	Metadata      Metadata     `json:"metadata"`
 	TotalContacts int          `json:"total"`
@@ -34,13 +33,13 @@ type ContactList struct {
 }
 
 type DeputyContact struct {
-	ContactId        int
+	Id               int
 	Name             string
 	JobTitle         string
 	Email            string
 	PhoneNumber      string
 	OtherPhoneNumber string
-	ContactNotes     string
+	Notes            string
 	IsMainContact    bool
 	IsNamedDeputy    bool
 }
@@ -49,7 +48,8 @@ type DeputyContactsDetails []DeputyContact
 
 func (c *Client) GetDeputyContacts(ctx Context, deputyId, displayContactLimit, search int, deputyType, columnBeingSorted, sortOrder string) (ContactList, AriaSorting, error) {
 	var contactList ContactList
-	var apiContactList ApiContactList
+	//var apiContactList ApiContactList
+	var apiContacts []ApiContact
 
 	req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/deputies/%d/contacts?&limit=%d&page=%d", deputyId, displayContactLimit, search), nil)
 
@@ -72,26 +72,26 @@ func (c *Client) GetDeputyContacts(ctx Context, deputyId, displayContactLimit, s
 		return contactList, AriaSorting{}, newStatusError(resp)
 	}
 
-	if err = json.NewDecoder(resp.Body).Decode(&apiContactList); err != nil {
+	if err = json.NewDecoder(resp.Body).Decode(&apiContacts); err != nil {
 		return contactList, AriaSorting{}, err
 	}
 
 	var contacts DeputyContactsDetails
-	for _, t := range apiContactList.Contacts {
+	for _, t := range apiContacts {
 
-			var contact = DeputyContact{
-				ContactId:        t.ContactId,
-				Name:             t.Name,
-				JobTitle:         t.JobTitle,
-				Email:            t.Email,
-				PhoneNumber:      t.PhoneNumber,
-				OtherPhoneNumber: t.OtherPhoneNumber,
-				ContactNotes:     t.ContactNotes,
-				IsMainContact:    t.IsMainContact,
-				IsNamedDeputy:    t.IsNamedDeputy,
-			}
-			
-			contacts = append(contacts, contact)
+		var contact = DeputyContact{
+			Id:               t.Id,
+			Name:             t.Name,
+			JobTitle:         t.JobTitle,
+			Email:            t.Email,
+			PhoneNumber:      t.PhoneNumber,
+			OtherPhoneNumber: t.OtherPhoneNumber,
+			Notes:            t.Notes,
+			IsMainContact:    t.IsMainContact,
+			IsNamedDeputy:    t.IsNamedDeputy,
+		}
+
+		contacts = append(contacts, contact)
 	}
 	contactList.Contacts = contacts
 
