7646d96b45a5c9812318bed7fcab1710debc5c4a
Sam Joseph <denialanderror@users.noreply.github.com>
2023-08-30 10:40:53 +0100

    Sw 6739 axe (#342)
    * sw-6739: Axe added
    
    * sw-6739: Fixed contrast issue
    
    * sw-6379: Moved a11y tests into their own file
    
    * sw-6739: Adds back in acceptance testing step using grep tags
    
    ---------
    
    Co-authored-by: Nicholassully <31247234+Nicholassully@users.noreply.github.com>

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,3 +1,5 @@
+import navTabs from "../fixtures/navigation.json";
+
 describe("Navigation bar", () => {
     beforeEach(() => {
         cy.setCookie("Other", "other");
@@ -5,25 +7,15 @@ describe("Navigation bar", () => {
         cy.visit("/supervision/deputies/1");
     });
 
-    const expected = [
-        ["Deputy details", "/supervision/deputies/1"],
-        ["Contacts", "/supervision/deputies/1/contacts"],
-        ["Clients", "/supervision/deputies/1/clients"],
-        ["Timeline", "/supervision/deputies/1/timeline"],
-        ["Notes", "/supervision/deputies/1/notes"],
-        ["Tasks", "/supervision/deputies/1/tasks"],
-        ["Assurance visits", "/supervision/deputies/1/assurance-visits"],
-    ];
-
     it("has titles and working nav links for all tabs in the correct order", () => {
         cy.get(".moj-sub-navigation__list")
             .children()
             .each(($el, index) => {
-                cy.wrap($el).should("contain", expected[index][0]);
+                cy.wrap($el).should("contain", navTabs[index][0]);
                 cy.wrap($el)
                     .find("a")
                     .should("have.attr", "href")
-                    .and("contain", expected[index][1]);
+                    .and("contain", navTabs[index][1]);
             });
     });
 });
