ccf61c2b2bbe886b761ceffcffd70a91b6b5e6af
samuel.joseph <denialanderror@googlemail.com>
2023-10-27 13:47:33 +0100

    sw-6613: Updated pagination tests

--- cypress/e2e/pagination.cy.js
+++ cypress/e2e/pagination.cy.js
@@ -6,21 +6,18 @@ describe("Pagination", () => {
     });
 
     it("shows correct number of total clients", () => {
-        cy.get("#top-pagination").contains(".moj-pagination__results", 'Showing 1 to 3 of 3 clients')
-    })
+        cy.get("#top-pagination").contains(".moj-pagination__results", 'Showing 1 to 3 of 3 clients');
+    });
 
-    it("can select 25 from task view value dropdown", () => {
-        cy.get("#top-pagination .display-rows").select('25')
-            .invoke('val').should('contain', 'limit=25')
-    })
+    it("has the correct options in the per-page dropdown", () => {
+        cy.get("#top-pagination .display-rows option").then(options => {
+            const values = [...options].map(o => o.value);
+            expect(values).to.deep.eq(["?limit=25&page=1", "?limit=50&page=1", "?limit=100&page=1"]);
+        });
+    });
 
-    it("can select 50 from task view value dropdown", () => {
-        cy.get("#top-pagination .display-rows").select('50')
-            .invoke('val').should('contain', 'limit=50')
-    })
-
-    it("can select 100 from task view value dropdown", () => {
-        cy.get("#top-pagination .display-rows").select('100')
-            .invoke('val').should('contain', 'limit=100')
-    })
+    it("redirects page to new per-page limit", () => {
+        cy.get("#top-pagination .display-rows").select('50');
+        cy.location('search').should('contain', 'limit=50');
+    });
 });
