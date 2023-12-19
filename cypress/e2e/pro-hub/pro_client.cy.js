54968548a11aa67d42499cc8f92ef53df87e72b0
nicholassully <nicholas.sully@digital.justice.gov.uk>
2023-12-19 11:06:37 +0000

    sw-6609 cypress tests

--- cypress/e2e/pro-hub/pro_client.cy.js
+++ cypress/e2e/pro-hub/pro_client.cy.js
@@ -29,84 +29,44 @@ describe("Clients tab", () => {
             });
     });
 
-    // it("lists clients with active/closed/duplicate orders", () => {
-    //     cy.get(".govuk-table__body > .govuk-table__row").should(
-    //         "have.length",
-    //         3
-    //     );
-    // });
-    //
-    // it("Clients surname have been sorted in order of ascending by default", () => {
-    //     cy.get(":nth-child(1) > .client_name_ref > .govuk-link").should(
-    //         "contain",
-    //         "Burgundy"
-    //     );
-    //     cy.get(":nth-child(2) > .client_name_ref > .govuk-link").should(
-    //         "contain",
-    //         "Dauphin"
-    //     );
-    //     cy.get(":nth-child(3) > .client_name_ref > .govuk-link").should(
-    //         "contain",
-    //         "Here"
-    //     );
-    // });
-    //
-    // it("Clients surname have been sorted in order of descending", () => {
-    //     cy.get('[label="sort-name-column-ascending"] > a').click();
-    //     cy.get(":nth-child(1) > .client_name_ref > .govuk-link").should(
-    //         "contain",
-    //         "Here"
-    //     );
-    //     cy.get(":nth-child(2) > .client_name_ref > .govuk-link").should(
-    //         "contain",
-    //         "Dauphin"
-    //     );
-    //     cy.get(":nth-child(3) > .client_name_ref > .govuk-link").should(
-    //         "contain",
-    //         "Burgundy"
-    //     );
-    // });
-    //
-    // it("Clients report due dates have been sorted in order of ascending", () => {
-    //     cy.get('[label="sort-due-date-column-none"] > a').click();
-    //     cy.get(":nth-child(1) > .reports").should("contain", "21/12/2015");
-    //     cy.get(":nth-child(2) > .reports").should("contain", "01/10/2018");
-    //     cy.get(":nth-child(3) > .reports").should("contain", "-");
-    // });
-    //
-    // it("Clients report due dates have been sorted in order of descending", () => {
-    //     cy.get('[label="sort-due-date-column-none"] > a').click();
-    //     cy.get('[label="sort-due-date-column-ascending"] > a').click();
-    //     cy.get(":nth-child(1) > .reports").should("contain", "-");
-    //     cy.get(":nth-child(2) > .reports").should("contain", "01/10/2018");
-    //     cy.get(":nth-child(3) > .reports").should("contain", "21/12/2015");
-    // });
-    //
-    // it("Clients risk have been sorted in order of ascending", () => {
-    //     cy.get('[label="sort-aria-column-none"] > a').click();
-    //     cy.get(":nth-child(1) > .data-crec").should("contain", "2");
-    //     cy.get(":nth-child(2) > .data-crec").should("contain", "3");
-    //     cy.get(":nth-child(3) > .data-crec").should("contain", "4");
-    // });
-    //
-    // it("Clients risk have been sorted in order of descending", () => {
-    //     cy.get('[label="sort-aria-column-none"] > a').click();
-    //     cy.get('[label="sort-aria-column-ascending"] > a').click();
-    //     cy.get(":nth-child(1) > .data-crec").should("contain", "4");
-    //     cy.get(":nth-child(2) > .data-crec").should("contain", "3");
-    //     cy.get(":nth-child(3) > .data-crec").should("contain", "2");
-    // });
-    //
-    // it("Clients have a latest visit recorded", () => {
-    //     cy.get(":nth-child(1) > .visit_type").should("contain", "01/01/2000");
-    //     cy.get(":nth-child(1) > .visit_type").should(
-    //         "contain",
-    //         "Standard visit"
-    //     );
-    //     cy.get(":nth-child(1) > .visit_type").should("contain", "Green");
-    //     cy.get(":nth-child(2) > .visit_type").should("contain", "03/03/2020");
-    //     cy.get(":nth-child(2) > .visit_type").should("contain", "Urgent visit");
-    //     cy.get(":nth-child(2) > .visit_type").should("contain", "Red");
-    //     cy.get(":nth-child(3) > .visit_type").should("contain", "-");
-    // });
+    it("lists clients with active/closed/duplicate orders", () => {
+        cy.get(".govuk-table__body > .govuk-table__row").should(
+            "have.length",
+            3
+        );
+    });
+
+    it("Clients surname have been sorted in order of descending", () => {
+        cy.get('[aria-sort="ascending"] > a > button').click();
+        cy.url().should("contain", "order-by=surname&sort=desc");
+        cy.get('[aria-sort="descending"] > a > button').click();
+        cy.url().should("contain", "order-by=surname&sort=asc");
+    });
+
+    it("Clients report due have been sorted in order of descending", () => {
+        cy.get('a:contains("Report")').first().click()
+        cy.url().should("contain", "order-by=reportDue&sort=asc");
+        cy.get('a:contains("Report")').first().click()
+        cy.url().should("contain", "order-by=reportDue&sort=desc");
+    });
+
+    it("Clients risk have been sorted in order of descending", () => {
+        cy.get('a:contains("Risk")').first().click()
+        cy.url().should("contain", "order-by=crec&sort=asc");
+        cy.get('a:contains("Risk")').first().click()
+        cy.url().should("contain", "order-by=crec&sort=desc");
+    });
+
+    it("Clients have a latest visit recorded", () => {
+        cy.get(":nth-child(1) > .visit_type").should("contain", "01/01/2000");
+        cy.get(":nth-child(1) > .visit_type").should(
+            "contain",
+            "Standard visit"
+        );
+        cy.get(":nth-child(1) > .visit_type").should("contain", "Green");
+        cy.get(":nth-child(2) > .visit_type").should("contain", "03/03/2020");
+        cy.get(":nth-child(2) > .visit_type").should("contain", "Urgent visit");
+        cy.get(":nth-child(2) > .visit_type").should("contain", "Red");
+        cy.get(":nth-child(3) > .visit_type").should("contain", "-");
+    });
 });
