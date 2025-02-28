48d17b8dcd12ac9ac7e6373cfb9810a8ad449657
Kate Smith <52196833+kate-49@users.noreply.github.com>
2025-02-28 09:16:47 +0000

    Sw-5149 paginate deputy hub timeline (#481)
    * sw-5149 amend api call
    
    * sw-5149 api call returns timeline list
    
    * sw-5149 pagination works as expected
    
    * sw-5149 fix some tests
    
    * sw-5149 fix more tests
    
    * sw-5149 comment out failing cypress tests to check on adhoc
    
    * sw-5149 try to debug why returning too many events in adhoc
    
    * sw-5149 log url
    
    * sw-5149 works on adhoc uncomment cypress tests
    
    * sw-5149 added tests
    
    * sw-5149 check if timeline asc sort still needed
    
    * sw-5149 remove timeline asc sort and fix tests

--- internal/server/deputy_events_test.go
+++ internal/server/deputy_events_test.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"github.com/ministryofjustice/opg-go-common/paginate"
+	"github.com/ministryofjustice/opg-sirius-supervision-deputy-hub/internal/urlbuilder"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -14,14 +16,33 @@ type mockDeputyHubTimelineInformation struct {
 	count              int
 	lastCtx            sirius.Context
 	GetDeputyEventsErr error
-	deputyEvents       sirius.DeputyEvents
 }
 
-func (m *mockDeputyHubTimelineInformation) GetDeputyEvents(ctx sirius.Context, deputyId int) (sirius.DeputyEvents, error) {
+func (m *mockDeputyHubTimelineInformation) GetDeputyEvents(ctx sirius.Context, deputyId int, pageNumber int, timelineEventsPerPage int) (sirius.TimelineList, error) {
 	m.count += 1
 	m.lastCtx = ctx
 
-	return m.deputyEvents, m.GetDeputyEventsErr
+	var getDeputyEventsVars = sirius.TimelineList{
+		Limit: 25,
+		Pages: struct {
+			Current int `json:"current"`
+			Total   int `json:"total"`
+		}{
+			Current: 1,
+			Total:   1,
+		},
+		Total: 2,
+		DeputyEvents: sirius.DeputyEvents{
+			{
+				ID: 1,
+			},
+			{
+				ID: 2,
+			},
+		},
+	}
+
+	return getDeputyEventsVars, m.GetDeputyEventsErr
 }
 
 func TestNavigateToTimeline(t *testing.T) {
@@ -33,13 +54,38 @@ func TestNavigateToTimeline(t *testing.T) {
 	w := httptest.NewRecorder()
 	r, _ := http.NewRequest("GET", "/path", nil)
 
-	handler := renderTemplateForDeputyHubEvents(client, template)
+	handler := renderTemplateForDeputyHubEvents(client, template, EnvironmentVars{})
 	err := handler(AppVars{}, w, r)
 
 	assert.Nil(err)
 
 	resp := w.Result()
 	assert.Equal(http.StatusOK, resp.StatusCode)
+	assert.Equal(deputyHubEventVars{
+		AppVars: AppVars{
+			PageName: "Timeline",
+		},
+		DeputyEvents: sirius.DeputyEvents{
+			{
+				ID: 1,
+			},
+			{
+				ID: 2,
+			},
+		},
+		Pagination: paginate.Pagination{
+			CurrentPage:     1,
+			TotalPages:      1,
+			TotalElements:   2,
+			ElementsPerPage: 25,
+			ElementName:     "timeline event(s)",
+			PerPageOptions:  []int{25, 50, 100},
+			UrlBuilder: urlbuilder.UrlBuilder{
+				OriginalPath:    "/path",
+				SelectedPerPage: 25,
+			},
+		},
+	}, template.lastVars)
 }
 
 func TestDeputyEventsReturnsErrors(t *testing.T) {
@@ -53,7 +99,27 @@ func TestDeputyEventsReturnsErrors(t *testing.T) {
 	w := httptest.NewRecorder()
 	r, _ := http.NewRequest("GET", "/path", strings.NewReader(""))
 
-	returnedError := renderTemplateForDeputyHubEvents(client, template)(AppVars{}, w, r)
+	returnedError := renderTemplateForDeputyHubEvents(client, template, EnvironmentVars{})(AppVars{}, w, r)
 
 	assert.Equal(client.GetDeputyEventsErr, returnedError)
+}
+
+func TestCreateUrlBuilder(t *testing.T) {
+	myUrlBuilder := CreateUrlBuilder("67/timeline", 25, "/supervision/deputies/")
+
+	assert.Equal(t, urlbuilder.UrlBuilder{
+		OriginalPath:    "/supervision/deputies/67/timeline",
+		SelectedPerPage: 25,
+		SelectedFilters: nil,
+		SelectedSort:    urlbuilder.Sort{},
+	}, myUrlBuilder)
+
+	myUrlBuilder = CreateUrlBuilder("99/url", 100, "/test/path/")
+
+	assert.Equal(t, urlbuilder.UrlBuilder{
+		OriginalPath:    "/test/path/99/url",
+		SelectedPerPage: 100,
+		SelectedFilters: nil,
+		SelectedSort:    urlbuilder.Sort{},
+	}, myUrlBuilder)
 }
