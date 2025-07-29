ed8ba387e1bb70d4feeb2e443a78c87ea5422143
nicholassully <nicholas.sully@digital.justice.gov.uk>
2025-07-29 07:38:08 +0100

    sfs-58 added accessibility statement

---
+++ cypress/e2e/footer.cy.js
@@ -0,0 +1,9 @@
+describe("Footer", () => {
+    beforeEach(() => {
+        cy.visit("/supervision/deputies/1");
+    });
+
+    it("should show the accessibility link", () => {
+        cy.get('[data-cy="accessibilityStatement"]').should('contain', 'Accessibility statement')
+    });
+});
